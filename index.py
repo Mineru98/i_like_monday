da06be98e42be533e3ae42c6875ca63c01b947cf
iamiks <rmstjr1030@naver.com>
2022-06-24 20:56:15 +0900

    Update Code

--- index.py
+++ index.py
@@ -1,15 +1,17 @@
 # -*- coding:utf-8 -*-
 import os
 import time
+import pathlib
 import schedule
 import multiprocessing
 from playsound import playsound
 
 def play_monday():
-    play = multiprocessing.Process(target=playsound, args=(os.path.join(".", "assets", "spongebob.mp3"),))
+    play = multiprocessing.Process(target=playsound, args=(os.path.join(pathlib.Path(__file__).parent.absolute(), "assets", "spongebob.mp3"),))
     play.start()
 
 schedule.every().monday.at('10:30').do(play_monday)
+schedule.every().friday.at('20:58').do(play_monday)
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
